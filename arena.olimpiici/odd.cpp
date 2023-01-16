b7004d3ffa8fb368a5529b89d25084e1f2c9ae5e
angelator312 <angel.raev2012@gmail.com>
2023-01-16 20:32:19 +0200

    odd and ignore it

--- arena.olimpiici/odd.cpp
+++ arena.olimpiici/odd.cpp
@@ -6,11 +6,13 @@ int main(){
     for (long long i = in; i <= in2; i++){
         long dels;
         dels=0;
-        for (long long j = 1;j<=i;j++){
+        for (long long j = 2;j<=i/2;j++){
             if (i%j==0){
                 dels++;
             }
         }
+        dels++;
+        dels++;
         if (dels%2==1){
             out++;
         }
