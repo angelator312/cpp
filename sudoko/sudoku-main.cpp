dbd0498cfe58ea0925cd203a615f40d636bd822d
angelator312 <angel.raev2012@gmail.com>
2023-09-09 14:09:04 +0300

    sudoku-fastAlgorithm

---
+++ sudoko/sudoku-main.cpp
@@ -0,0 +1,25 @@
+#include "Board.hpp"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  Board sudoku(string(argv[1]) + ".out.fastSudoku");
+  if (argc >= 2) {
+    fstream s(argv[1], s.in);
+    sudoku.read(s); 
+  } else {
+    sudoku.read(cin);
+  }
+  sudoku.write();
+  bool c = string(argv[2]) == "count";
+  if (c) {
+    sudoku.countM = true;
+  };
+  sudoku.algorithm();
+  if (c)
+  {
+    sudoku.out<<"Sudoku solutions : "<<sudoku.count<<endl;
+  }else{
+    sudoku.write();
+  };
+  return 0;
+}
