e5b2e7a95e2255febce1e977e331392d899f3c30
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-10-05 12:50:02 +0300

    more

--- EJOY/2026/grader67/grader67Grader.cpp
+++ EJOY/2026/grader67/grader67Grader.cpp
@@ -1,4 +1,5 @@
 #include "grader67.h"
+#include <climits>
 #include <iostream>
 #include <unordered_set>
 using namespace std;
@@ -9,15 +10,21 @@ int main() {
   int sum = 0;
   double out = 0;
   unordered_set<int> arr;
+  int mx = 0, mn = INT_MAX;
   for (int e : ans) {
     arr.insert(e);
     sum = (sum + e) % n;
+    mx = max(mx, e);
+    mn = min(mn, e);
   }
   if (arr.size() > 1)
     out += 0.15;
-  else
-    out += 0.60;
+
   if (sum == 0)
     out += 0.25;
+
+  if (abs(mn - mx) <= 2)
+    out += 0.60;
+
   cout << out << endl;
 }
--- EJOY/2026/permutation/permutation.cpp
+++ EJOY/2026/permutation/permutation.cpp
@@ -1,15 +1,40 @@
 #include "permutation.h"
-#include <bits/stdc++.h>
+#include <cstdlib>
+#include <vector>
 using namespace std;
-vector<int> encode(vector<int> p) {
-  for (int i = 0; i < p.size(); ++i)
-    if (p[i] == i + 1)
-      p[i] = -1;
-  return p;
+vector<int> encode(const vector<int> p) {
+  vector<int> p2;
+  int cnt = 0;
+  p2.push_back(p[0]);
+  for (int i = 1; i < p.size(); ++i) {
+    if (p[i] == p[i - 1] + 1) {
+      ++cnt;
+      p2.push_back(-1);
+    } else
+      p2.push_back(p[i]);
+  }
+  if (cnt == 0) {
+    for (int &i : p2)
+      if (i == 1)
+        i = -1;
+    if (p2[0] > 0)
+      p2[0] *= -1;
+  }
+  return p2;
 }
 vector<int> decode(vector<int> p) {
-  for (int i = 0; i < p.size(); ++i)
-    if (p[i] == -1)
-      p[i] = 1 + i;
+
+  int cnt = 0;
+  for (int i = 1; i < p.size(); ++i)
+    if (p[i] == -1) {
+      ++cnt;
+      if (p[0] > 0)
+        p[i] = 1 + p[i - 1];
+      else
+        p[i] = 1;
+    }
+  if (cnt == 0)
+    p[0] = 1;
+  p[0] = abs(p[0]);
   return p;
 }
