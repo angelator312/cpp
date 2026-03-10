80421b531f43a6ff41db28d218791d1ee02c88b1
Angel Raev <112373005+angelator312@users.noreply.github.com>
2026-03-10 13:45:16 +0000

    Add 2022-C1.cpp (100 points)

--- arena.olimpiici/NOI-3-C/2022-C1.cpp
+++ arena.olimpiici/NOI-3-C/2022-C1.cpp
@@ -1,50 +1,59 @@
-#include <stdio.h>
-#define endl "\n"
-// using namespace std;
-int left, right, up, down;
-inline void read() {
-  char ch = getchar();
-  while (true) {
-    if (ch == 'L')
-      --left;
-    else if (ch == 'R')
-      ++left;
-    else if (ch == 'U')
-      --up;
-    else if (ch == 'D')
-      ++up;
-    else
-      break;
-    ch = getchar();
-  }
+// https://arena.olimpiici.com/#/catalog/802/problem/102122
+#include <array>
+#include <bits/stdc++.h>
+#include <utility>
+using namespace std;
+const double xC = 0, ERR = 1e-6;
+struct pii
+{
+  double x, y;
+}; // format-off
+inline double sq(double a) { return a * a; }
+double dist(pii A, pii B) { return sqrt(sq(A.x - B.x) + sq(A.y - B.y)); }
+double area(pii A, pii B, pii C)
+{
+  return abs((B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y));
 }
-int main() {
-  read();
-  if (left == 0 && up == 0) {
-    printf("0");
-    return 0;
+pair<pii, pii> lrFromPoints(pii A, pii B)
+{
+  return {{min(A.x, B.x), min(A.y, B.y)}, {max(A.x, B.x), max(A.y, B.y)}};
+}
+pii mid(pii A, pii B)
+{
+  return {(A.x + B.x) / 2.0,
+          (A.y + B.y) / 2.0};
+}
+pii SolveFor(pii P, pii A, pii B, double target)
+{
+  pii B1 = B;
+  while(dist(A,B)>ERR)
+  {
+    pii M = mid(A, B);
+    double areaBMP = area(B1, M, P);
+    // cout << std::fixed << std::setprecision(6) << P.x << " " << P.y << " " << A.x << " " << A.y << " " << B.x << " " << B.y << " " << target << " :: " << areaBMP << endl;
+    if (areaBMP > target)
+      A = M;
+    else
+      B = M;
   }
-  if (left < 0) {
-    right = -left;
-    left = 0;
-  }
-  if (up < 0) {
-    down = -up;
-    up = 0;
-  }
-  //   std::cerr << left << " " << right << " " << up << " " << down << endl;
-  while (up--)
-    printf("U");
-  while (down--)
-    printf("D");
-  while (left--)
-    printf("L");
-  while (right--)
-    printf("R");
-  printf(endl);
-  //   std::couts << std::string(up, 'U');
-  //   std::cout << std::string(down, 'D');
-  //   std::cout << std::string(left, 'L');
-  //   std::cout << std::string(right, 'R') << "\n";
-  return 0;
+  return mid(A, B);
+}
+pii pC, pB, pP, pA;
+int main()
+{
+  pC.x = 0;
+  cin >> pC.y >> pB.x >> pB.y >> pP.x >> pP.y;
+  pii ansG;
+  if (area(pA, pB, pP) == 0)
+    swap(pA, pC);
+  if (area(pA, pC, pP) == 0)
+    swap(pA, pB);
+  double areaABC = area(pA, pB, pC);
+  double areaABP = area(pA, pB, pP);
+  double areaACP = area(pA, pC, pP);
+  if (areaABP < areaACP)
+    ansG = SolveFor(pP, pA, pC, areaABC / 2.0);
+  else
+    ansG = SolveFor(pP, pA, pB, areaABC / 2.0);
+  cout << fixed << setprecision(6) << ansG.x << " " << ansG.y << endl;
 }
